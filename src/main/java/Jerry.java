54bf5c85e4504eb3893513abef368fe34bc6938f
e1384467 <e1384467@u.nus.edu>
2026-01-20 19:24:59 +0800

    Added Echo feature to Jerry class
    Jerry will now echo back whatever the user types.

--- src/main/java/Jerry.java
+++ src/main/java/Jerry.java
@@ -1,10 +1,27 @@
+import java.util.Scanner;
+
 public class Jerry {
 
     public static final String CHATBOT_NAME = "Jerry";
+    public static final String EXIT = "bye";
+
+    public static void echo() {
+        Scanner scan = new Scanner(System.in);
+        String userInput;
+        while (true) {
+            System.out.print("User input: ");
+            userInput = scan.nextLine();
+            if (userInput.equalsIgnoreCase(EXIT)) {
+                break;
+            }
+            System.out.println(CHATBOT_NAME + ": " + userInput + "\n");
+        }
+    }
 
     public static void main(String[] args) {
         System.out.println("Hello! I'm " + CHATBOT_NAME);
         System.out.println("What can I do for you?\n");
+        echo();
         System.out.println("Bye. Hope to see you again soon!");
     }
 }
