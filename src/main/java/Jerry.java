09c2e3bf0b453c1746c6ebf61954c95fdd6c3785
e1384467 <e1384467@u.nus.edu>
2026-01-20 20:20:50 +0800

    Added support for a list of task in Jerry class
    Added a taskList ArrayList variable to hold all the tasks
    
    Added a printList() method to print the current tasks
    
    Change the previous echo method name to addTask, and modified it to add
    the user's task
    
    Minor changes/addition:
    - added second keyword, 'list'. This is the keyword by the
    user to list out the tasks
    - change addTask() and printList() to be instance method, thus a
      constructor is needed for jerry
    
    Jerry can now add task inputted by the user to an ArrayList and display
    back that list to the user

--- src/main/java/Jerry.java
+++ src/main/java/Jerry.java
@@ -1,11 +1,26 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Jerry {
 
     public static final String CHATBOT_NAME = "Jerry";
     public static final String EXIT = "bye";
+    public static final String SHOW_LIST = "list";
+    private final ArrayList<String> taskList;
 
-    public static void echo() {
+    public Jerry() {
+        this.taskList = new ArrayList<>();
+    }
+
+    public void printList() {
+        System.out.println("Your list:");
+        for (int index = 0; index < this.taskList.size(); index += 1) {
+            System.out.println(index + 1 + ". " + this.taskList.get(index));
+        }
+        System.out.println();
+    }
+
+    public void addTask() {
         Scanner scan = new Scanner(System.in);
         String userInput;
         while (true) {
@@ -14,14 +29,29 @@ public class Jerry {
             if (userInput.equalsIgnoreCase(EXIT)) {
                 break;
             }
-            System.out.println(CHATBOT_NAME + ": " + userInput + "\n");
+            if (userInput.equalsIgnoreCase(SHOW_LIST)) {
+                if (this.taskList.isEmpty())
+                {
+                    System.out.println(CHATBOT_NAME +
+                            ": your list is currently empty. Type to add more!\n");
+                } else {
+                    printList();
+                }
+                continue;
+            }
+            this.taskList.add(userInput);
+            System.out.println(CHATBOT_NAME +
+                    ": I have added " +
+                    userInput +
+                    " to your list!\n");
         }
     }
 
     public static void main(String[] args) {
+        Jerry jerry = new Jerry();
         System.out.println("Hello! I'm " + CHATBOT_NAME);
         System.out.println("What can I do for you?\n");
-        echo();
+        jerry.addTask();
         System.out.println("Bye. Hope to see you again soon!");
     }
 }
