bc3cc17a26f0a32b6e179b8e432e878d337efc7f
e1384467 <e1384467@u.nus.edu>
2026-01-20 16:26:19 +0800

    Change chatbot name to Jerry and removed all traces of Duke

---
+++ src/main/java/Jerry.java
@@ -0,0 +1,10 @@
+public class Jerry {
+
+    public static final String CHATBOT_NAME = "Jerry";
+
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm " + CHATBOT_NAME);
+        System.out.println("What can I do for you?\n");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
