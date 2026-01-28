f0f6aa4c7542426808aa24d4dc42851242007960
e1384467 <e1384467@u.nus.edu>
2026-01-28 23:01:39 +0800

    Handle data/ directory not existing exception

--- src/main/java/Storage.java
+++ src/main/java/Storage.java
@@ -12,16 +12,29 @@ public class Storage {
     public static ArrayList<Task> initialise() throws JerryException {
        try {
            File taskFile =  new File(FILE_PATH.toString());
+           File parentDirectory = new File(taskFile.getParent());
+
+           if (!parentDirectory.exists()) {
+               if (!parentDirectory.mkdirs()){
+                   throw new FileErrorException("Failed trying to create data/ directory.\n"
+                           + "Please ensure that the folder is writable.");
+               }
+           }
+
            ArrayList<Task> taskList = new ArrayList<Task>();
            if (taskFile.createNewFile()) {
-               return taskList;
+                return taskList;
            }
            return Parser.loadTasksFromFile(taskFile, taskList);
        } catch (FileNotFoundException e) {
-           throw new MissingFileException("Oh noo!!! Jerry.txt file is missing or stored in the wrong directory.\n"
-                   + "Make sure to use data/ directory or restart me.\n");
+           throw new MissingFileException("Oh noo!!! Jerry.txt file is missing or inaccessible.\n"
+                   + "Please make sure that Jerry.txt is in the data/ directory and that it is writable.\n");
        } catch (IOException e) {
-           throw new FileErrorException("There seems to be an error when creating Jerry.txt.\n");
+           throw new FileErrorException("There is an I/O error when creating Jerry.txt.\n"
+                   + "Please make sure that the folder is writable.\n");
+       } catch (SecurityException e) {
+           throw new FileErrorException("There seems to be some issue with the permissions of the folder/file.\n"
+                   + "Please make sure that the folder/file is writable.\n");
        }
 
     }
@@ -34,7 +47,8 @@ public class Storage {
             }
             taskFile.close();
         } catch (IOException e) {
-            throw new FileErrorException("There seems to be an error when writing to Jerry.txt.\n");
+            throw new FileErrorException("There seems to be an error when writing to Jerry.txt.\n"
+                    + "Please make sure that Jerry.txt is writable.\n");
         }
     }
 }
