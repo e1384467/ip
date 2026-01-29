e13e69c077cac7a9f9ad7eeb070335529909c600
e1384467 <e1384467@u.nus.edu>
2026-01-29 22:21:57 +0800

    Add Javadoc for Storage class

--- src/main/java/jerry/storage/Storage.java
+++ src/main/java/jerry/storage/Storage.java
@@ -11,10 +11,21 @@ import java.io.IOException;
 import java.nio.file.Path;
 import java.util.ArrayList;
 
+/**
+ * Handles loading and saving of task data to storage.
+ */
 public class Storage {
 
+    /** Path to the file used for storing task data. */
     private final static Path FILE_PATH = Path.of("data/Jerry.txt");
 
+    /**
+     * Returns a list of tasks loaded from the saved data file.
+     * If the data file does not exist, it is created and an empty list is returned.
+     *
+     * @return A list of tasks loaded from storage.
+     * @throws JerryException If the file cannot be created, accessed, or contains corrupted data.
+     */
     public static ArrayList<Task> initialise() throws JerryException {
        try {
            File taskFile =  new File(FILE_PATH.toString());
@@ -42,6 +53,13 @@ public class Storage {
 
     }
 
+    /**
+     * Saves the given task list to the data file in the expected file format.
+     * Existing file contents will be overwritten.
+     *
+     * @param taskList The task list to be saved to storage.
+     * @throws JerryException If an error occurs while writing to the data file or creating the parent directory
+     */
     public static void save(TaskList taskList) throws JerryException {
         try {
             File taskFile =  new File(FILE_PATH.toString());
