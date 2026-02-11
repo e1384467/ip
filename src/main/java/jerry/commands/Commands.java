1121f344f31bca2ee50b9333721010ed0446fe54
e1384467 <e1384467@u.nus.edu>
2026-02-11 23:16:21 +0800

    Commands: Implement abstract execute method
    Execution logic for each command is currently handled in
    the Jerry class using switch-case statements.
    
    This reduces extensibility, as adding a new command
    requires modifying Jerry.
    
    Introduce an abstract execute() method in the Commands
    enum and implement it for each constant.

--- src/main/java/jerry/commands/Commands.java
+++ src/main/java/jerry/commands/Commands.java
@@ -1,21 +1,99 @@
 package jerry.commands;
 
 import jerry.exceptions.InvalidCommandException;
+import jerry.exceptions.JerryException;
+import jerry.parser.Parser;
+import jerry.storage.Storage;
+import jerry.task.Task;
+import jerry.task.TaskList;
+import jerry.ui.Ui;
 
 /**
  * Represents the set of valid command keywords supported by Jerry.
  * Each enum constant corresponds to a command a user can enter (e.g., {@code todo}, {@code list}).
  */
 public enum Commands {
-    BYE,
-    LIST,
-    MARK,
-    UNMARK,
-    TODO,
-    DEADLINE,
-    EVENT,
-    DELETE,
-    FIND;
+    BYE {
+        @Override
+        public String execute(TaskList taskList, Ui ui, String userInputArguments) throws JerryException {
+            Storage.writeTasksToFile(taskList);
+            return ui.showBye();
+        }
+    },
+    LIST{
+        @Override
+        public String execute(TaskList taskList, Ui ui, String userInputArguments) throws JerryException {
+            return ui.displayList(taskList);
+        }
+    },
+    MARK{
+        @Override
+        public String execute(TaskList taskList, Ui ui, String userInputArguments) throws JerryException {
+            int arrayIndex = Parser.getArrayIndex(userInputArguments);
+            Task markTask = taskList.markTask(arrayIndex);
+
+            Storage.writeTasksToFile(taskList);
+            return ui.showMark(markTask);
+        }
+    },
+    UNMARK{
+        @Override
+        public String execute(TaskList taskList, Ui ui, String userInputArguments) throws JerryException {
+            int arrayIndex = Parser.getArrayIndex(userInputArguments);
+            Task unmarkTask = taskList.unmarkTask(arrayIndex);
+
+            Storage.writeTasksToFile(taskList);
+            return ui.showUnmark(unmarkTask);
+        }
+    },
+    TODO{
+        @Override
+        public String execute(TaskList taskList, Ui ui, String userInputArguments) throws JerryException {
+            Task todoTask = Parser.parseTodo(userInputArguments);
+            taskList.add(todoTask);
+
+            Storage.writeTasksToFile(taskList);
+            return ui.showAdd(todoTask, taskList.size());
+        }
+    },
+    DEADLINE{
+        @Override
+        public String execute(TaskList taskList, Ui ui, String userInputArguments) throws JerryException {
+            Task deadlineTask = Parser.parseDeadline(userInputArguments);
+            taskList.add(deadlineTask);
+
+            Storage.writeTasksToFile(taskList);
+            return ui.showAdd(deadlineTask, taskList.size());
+        }
+    },
+    EVENT{
+        @Override
+        public String execute(TaskList taskList, Ui ui, String userInputArguments) throws JerryException {
+            Task eventTask = Parser.parseEvent(userInputArguments);
+            taskList.add(eventTask);
+
+            Storage.writeTasksToFile(taskList);
+            return ui.showAdd(eventTask, taskList.size());
+        }
+    },
+    DELETE{
+        @Override
+        public String execute(TaskList taskList, Ui ui, String userInputArguments) throws JerryException {
+            int arrayIndex = Parser.getArrayIndex(userInputArguments);
+            Task deletedTask = taskList.deleteTask(arrayIndex);
+
+            Storage.writeTasksToFile(taskList);
+            return ui.showDelete(deletedTask, taskList.size());
+        }
+    },
+    FIND{
+        @Override
+        public String execute(TaskList taskList, Ui ui, String userInputArguments) throws JerryException {
+            String searchQuery = Parser.getSearchQuery(userInputArguments);
+            TaskList possibleResults = taskList.find(searchQuery);
+            return ui.displayList(possibleResults);
+        }
+    };
 
     /**
      * Returns the {@code Commands} enum that matches the given user command input.
@@ -32,4 +110,6 @@ public enum Commands {
             throw new InvalidCommandException();
         }
     }
+
+    public abstract String execute(TaskList taskList, Ui ui, String userInputArguments) throws JerryException;
 }
